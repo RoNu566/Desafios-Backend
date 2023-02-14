54439917b8ebe37c7d1c401ca94f52ad1eb3fead
RoNu566 <rocion5666@gmail.com>
2023-02-14 20:35:50 -0300

    segundo desafio entregable

--- entregable.js
+++ entregable.js
@@ -1,50 +1,94 @@
+const fs  = require("fs");
+
 class ProductManager{
-    products = [];
-    static Contador = 0;
+    #Contador = 1;
+    #path = ""
 
-    constructor(title,description,price,thumbnail,code,stock){
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.thumbnail = thumbnail;
-        this.code = code;
-        this.stock = stock;
-        this.id = ProductManager.Contador
+    constructor(path){
+        this.#path = path;
     }
 
-    getProducts(){
-        console.table (this.products)
-        return this.products;
+    async getProducts(){
+        try{
+            const products = await fs.promises.readFile(this.#path, "utf-8"); 
+            return JSON.parse(products)
+        }catch(error){
+            return [];
+        }
     }
 
-    getProductById(id){
-       
-        if (this.products.find(product => product.id == id)!=undefined){
-            return this.products.find(product => product.id == id)
+    async getProductById(id){
+        const products = await this.getProducts();
+        if (products.find(product => product.id == id)){
+            return products.find((product) => product.id == id)
         }else{
-            return "Product Not found";
+            console.log("Product Not found");
         }
     }
 
-    addProduct(title,description,price,thumbnail,code,stock){
-        if (!title || !description || !price || !thumbnail || !code || !stock){
-        console.log("Faltan cargar Paramentros")    
-        }
-        else{
-            const productoRepetido = this.products.find((prod)=>prod.code == code)
-            if (!productoRepetido){
-                ProductManager.Contador += 1;
-                const newProduct = new ProductManager (title,description,price,thumbnail,code,stock);
-                this.products = [...this.products, newProduct];
-                console.log (`Producto ${title} agregado con exito y con id generado ${this.id}`);
-                this.getProducts();
+    async addProduct(title,price,thumbnail,code,stock){
+        const products = await this.getProducts();
+
+        const newProduct = {
+            id : this.#Contador,
+            title,
+            price,
+            thumbnail,
+            code,
+            stock,
+        };
+        if (!title || !price || !thumbnail || !code || !stock){
+            console.log("Faltan cargar Paramentros")    
             }
             else{
-                console.log (`El producto con el código ${code} ya se encuentra cargado, modifique el codigo`)
+                const productoRepetido = products.find((prod) => prod.code == code)
+                if (!productoRepetido){;
+                    await fs.promises.writeFile(this.#path, JSON.stringify([...products, newProduct]))
+                    console.log (`Producto ${title} agregado con exito y con id generado ${this.#Contador}`)
+                    this.#Contador += 1;
                 }
+                else{
+                    console.log (`El producto con el código ${code} ya se encuentra cargado, modifique el codigo`)
+                    }
+                }
+        }
+    
+    async updateProduct(id, propModify){
+        const products = await this.getProducts();
+        let auxiliar = products.find((prod) => prod.id == id);
+        if (!auxiliar){
+            console.log(`El producto con id: ${id} no se encuentra en la base de datos`)
+            }else{
+                auxiliar = {...auxiliar, ...propModify}
+                let newArray = products.filter((prod) => prod.id !== id);
+                newArray = [...newArray, auxiliar]
+                await fs.promises.writeFile(this.#path, JSON.stringify(newArray))
+                console.log ("Modificación exitosa")
             }
-    }
-}
+        }
 
-const manager1 = new ProductManager();
-const manager2 = new ProductManager();
+    async deleteProduct(id){
+        const products = await this.getProducts();
+        const auxiliar = products.filter((prod) => prod.id !== id);
+        await fs.promises.writeFile(this.#path, JSON.stringify(auxiliar)) //reescribo el archivo
+                    console.log (`Se ha eliminado el producto con el id : ${id}`)
+        }
+
+    }
+
+/// Pruebas ///
+async function Test(){
+    const manager1 = new ProductManager("./products.json")
+    await manager1.addProduct("Gorro",400,"img",1,20);
+    console.table (await manager1.getProducts())
+    await manager1.addProduct("Zapatos",200,"img",1,20);
+    await manager1.addProduct("Bufanda",400,"img",2,20);
+    console.table (await manager1.getProducts());
+    console.table (await manager1.getProductById(1));
+    await manager1.updateProduct(1,{title: "Remera", price: 5000})
+    console.table (await manager1.getProducts());
+    await manager1.deleteProduct(2);
+    console.table (await manager1.getProducts());
+}
+Test();
+
